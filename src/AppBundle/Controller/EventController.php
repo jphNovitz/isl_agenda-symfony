12ce68fb708b2a91b993c4917fb5ee6b102bff15
Jeanphinov <novitz@gmail.com>
2016-08-21 23:34:52 +0200

    eventController updateAction && deleteAction avec paramConverter

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Event;
+use AppBundle\Form\EventType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Entity\Event;
 
 class EventController extends Controller {
 
@@ -40,7 +43,7 @@ class EventController extends Controller {
      */
     public function addAction(Request $request) {
         $event = new Event();
-        $form = $this->createForm(\AppBundle\Form\EventType::class, $event);
+        $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $manager = $this->getDoctrine()->getManager();
@@ -48,7 +51,7 @@ class EventController extends Controller {
             $manager->flush();
             $id = $event->getId();
             $this->addFlash("success", "l'élement a bien été ajouté");
-            
+
             return $this->redirectToRoute('event_detail', ['id' => $id]);
         }
 
@@ -56,21 +59,82 @@ class EventController extends Controller {
     }
 
     /**
-     * @Route("/admin/event/update/", name="event_update")
+     * @Route("/admin/event/update/{id}")
+     *  requirements={"id": "\d+"},
+     *  defaults={"id": null},
+     *  name="event_update")
      */
-    public function updateAction() {
+    public function updateAction(Request $request, $id) {
 
+        try {
+            if (is_null($id)) {
+                throw new Exception(" Verifiez l'id");
+            }
+            if ($id <= 0) {
+                throw new Exception(" l'id fournie est impossible");
+            }
 
-        return $this->render();
+            /**
+             * Recuperation de l'objet evenement
+             */
+            $event = new Event();
+            $manager = $this->getDoctrine()->getManager();
+            $event = $manager->getRepository('AppBundle\Entity\Event')->find($id);
+            if (empty($event)) {
+                throw new Exception("l'élement est introuvable");
+            }
+            /**
+             * Creation du formulaire
+             */
+            $form = $this->createForm(EventType::class, $event); /* j'hydrate le formulaire avec l'objet $event */
+            $form->handleRequest($request);
+            /**
+             * Je passe la requete au formulaire, il la traite:
+             * - si le formulaire est "soumit" c'est qu'on arrive içi via un formulaire
+             * - si le forulaire est valid alors on est bon
+             */
+            if ($form->isSubmitted() && $form->isValid()) {
+                /**
+                 * detecte si pour arriver ici l'utilisateur a  cliqué sur le boutton supprimer
+                 * si oui je renvoie à la route qui s'occuppe de la suppression
+                 */
+                if ($form->get('supprimer')->isClicked()) {
+                    return $this->redirectToRoute("admin_event_delete", ['id' => $id]);
+                }
+
+                $manager->persist($event);
+                $manager->flush();
+
+                $this->addFlash("success", "l'élement a bien été modifié");
+            }
+            return $this->render('admin/event-form.html.twig', ['form' => $form->createView(), 'action' => 'modification']);
+        } catch (Exception $e) {
+            $this->addFlash('warning', "Impossible de trouver l'évènement (" . $e->getMessage() . ")");
+            return $this->redirectToRoute('admin_event_list');
+        }
     }
 
     /**
-     * @Route("/admin/event/delete/", name="event_delete")
+     * @Route("/admin/event/delete/{id}", name="admin_event_delete")
      */
-    public function deleteAction() {
+    public function deleteAction(Request $request, Event $event) {
 
+        $form = $this->createFormBuilder($event)
+                ->add('supprimer', SubmitType::class, ['label' => 'OUI Supprimer !'])
+                ->getForm();
 
-        return $this->render();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($event);
+            $manager->flush();
+            $this->addFlash("success", "l'élement a bien été supprimé");
+            return $this->redirectToRoute('event_list');
+            
+        }
+       
+        return $this->render('admin/form_delete_confirmation.html.twig', ['informations' => $event, 'form' => $form->createView()]);
     }
 
 }
