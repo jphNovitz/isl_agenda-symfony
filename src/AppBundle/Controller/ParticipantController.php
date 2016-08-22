9b6e61f4839a26e12ce08e50df552a60895db922
Jeanphinov <novitz@gmail.com>
2016-08-22 13:45:08 +0200

    ParticipantController -> refactor en utilisant le @ParamConverter

--- src/AppBundle/Controller/ParticipantController.php
+++ src/AppBundle/Controller/ParticipantController.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Isl Agenda Symfony / page ParticipantController.php
+ * Petit agenda d'évènement 
+ * Juin-Septembre 2016
+ * Novitz Jean-Philippe
+ * @jeanphinov on Github
+ * hello@jiphi.be 
+ * 
+ */
+
 namespace AppBundle\Controller;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -8,6 +18,7 @@ use AppBundle\Entity\Event;
 use AppBundle\Form\ParticipantType;
 use AppBundle\Form\EventType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -19,45 +30,58 @@ class ParticipantController extends Controller {
      * @Route("/participant/list/", name="participant_list")
      */
     public function listAction() {
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('AppBundle\Entity\Participant');
-        // $participants=$repo->findAll(); findAll() remplacé par myFindAll()
-        $participants = $repo->myFindAll();
+
+        $participants = $this->getListParticipant();
 
         return $this->render('public/participant-list.html.twig', ['participants' => $participants]);
     }
 
     /**
      * @Route("/participant/{id}", name="participant_detail")
+     * @ParamConverter("participant", class="AppBundle:Participant")
      */
-    public function detailAction($id) {
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('AppBundle\Entity\Participant');
-        $participant = $repo->find($id);
+    public function detailAction(Request $request, Participant $participant = null) {
 
-
-        return $this->render('public/participant-detail.html.twig', ['participant' => $participant]);
+        try {
+            if (empty($participant)) {
+                throw new Exception("L'élément n'existe pas");
+            }
+            return $this->render('public/participant-detail.html.twig', ['participant' => $participant]);
+        } catch (Exception $e) {
+            $this->addFlash('warning', $e->getMessage());
+            return $this->redirectToRoute('participant_list');
+        }
     }
 
     /**
      * @Route("/admin/participant/list/", name="admin_participant_list")
      */
     public function adminListAction() {
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('AppBundle\Entity\Participant');
-        $participants = $repo->myFindAll();
+        
+        $participants = $this->getListParticipant();
 
         return $this->render('admin/participant-list.html.twig', ['participants' => $participants]);
     }
 
     /**
-     * @Route("/admin/participant/add/", name="participant_add")
+     * @Route("/admin/participant/add/", name="admin_participant_add")
      */
     public function addAction(Request $request) {
 
+        /**
+         * addAction => démarche
+         *  - je crée le $form en utilisant un FormType
+         *  - le $form gère la requête et teste
+         *  - si j'arrive ici via le boutton submit d'un formulaire
+         *  - si le formulaire est valide
+         * 
+         * Si oui et oui alors je persist le nouvel élément dans la BD
+         * Si non c'est que je ne suis pas encore passé par un formulaire, je renvoie donc vers mon formulaire
+         * 
+         */
         $participant = new Participant();
         $form = $this->createForm(ParticipantType::class, $participant);
-        //$form->add('ajout', SubmitType::class, ['label' => 'Ajouter !', 'attr' => array('class' => 'btn btn-default')]);
+        // Fat Model Slim Controllers => je met le maximum concerant mon formumailre dans un fichier Type (il est fait pour cela)
 
         $form->handleRequest($request);
 
@@ -78,26 +102,12 @@ class ParticipantController extends Controller {
      * @Route("/admin/participant/update/{id}",
      *  requirements={"id": "\d+"},
      *  defaults={"id": null},
-     *  name="participant_update")
+     *  name="admin_participant_update")
+     * @ParamConverter("participant", class="AppBundle:Participant")
      */
-    public function updateAction(Request $request, $id) {
+    public function updateAction(Request $request, Participant $participant = null, $id) {
 
         try {
-            if (is_null($id)) {
-                throw new Exception("id ne peut etre null");
-                // je leve une exception si $id=null
-            }
-            if ($id == 0) {
-                throw new Exception("zero n'est pas un id valide");
-                // je leve une exception si $id=0
-            }
-
-            /* recuperation du participant correspondant à id */
-            $participant = new Participant();
-
-            $manager = $this->getDoctrine()->getManager();
-            $repo = $manager->getRepository('AppBundle\Entity\Participant');
-            $participant = $repo->find($id);
 
             if (empty($participant)) {
                 throw new Exception("L'élément n'existe pas");
@@ -115,8 +125,9 @@ class ParticipantController extends Controller {
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
                 if ($form->get('supprimer')->isClicked()) {
-                    return $this->redirectToRoute('participant_delete', ['id' => $id]);
+                    return $this->redirectToRoute('admin_participant_delete', ['id' => $id]);
                 }
+                $manager = $this->getDoctrine()->getManager();
                 $manager->persist($participant);
                 $manager->flush();
 
@@ -131,7 +142,6 @@ class ParticipantController extends Controller {
              */
             return $this->render('admin/participant-form.html.twig', ['form' => $form->createView(), 'action' => 'modification']);
         }
-
         /*
          *  je recupere l'exception levée je crée un message flash et re redirrige vers la liste en indiquant pourquoi
          */ catch (Exception $e) {
@@ -143,18 +153,19 @@ class ParticipantController extends Controller {
     }
 
     /**
-     * @Route("/admin/participant/delete/{id}", name="participant_delete")
+     * @Route("/admin/participant/delete/{id}", name="admin_participant_delete")
      */
-    public function deleteAction(Request $request, $id) {
-        $em = $this->getDoctrine()->getManager();
-        $participant = $em->getRepository('AppBundle\Entity\Participant')->find($id);
-
+    public function deleteAction(Request $request, Participant $participant) {
+        /**
+         * paramconverter recoit l'id et sait retrouver le participant concerné
+         */
         $form = $this->createFormBuilder($participant)
                 ->add('supprimer', SubmitType::class, ['label' => 'OUI Supprimer !'])
                 ->getForm();
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->remove($participant);
             $em->flush();
             $this->addFlash("success", "l'élement a bien été supprimé");
@@ -162,6 +173,19 @@ class ParticipantController extends Controller {
         }
 
         return $this->render('admin/form_delete_confirmation.html.twig', ['informations' => $participant, 'form' => $form->createView()]);
+    }
+    
+    /**
+     *  Fonction getListParticipant()
+     *  fonction qui sera utilisé pour créer les listes public et admin
+     *  je la met ici je n'ai plus qu'à l'utiliser pour mes deux Action
+     *  moins de code moins d'erreurs
+     */
+
+    public function getListParticipant() {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle\Entity\Participant');
+        return $repo->myFindAll();
     }
 
 }
